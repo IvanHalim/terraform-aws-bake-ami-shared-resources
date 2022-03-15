59fce5927302c61cba07c4b2337aed0df0875fde
ivanhalim <ivan.halim@traveloka.com>
2022-03-15 16:35:12 +0700

    Remove TODO comment

--- data.tf
+++ data.tf
@@ -318,14 +318,6 @@ data "aws_iam_policy_document" "codebuild_packer" {
       "iam:PassRole",
     ]
 
-    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-    # force an interpolation expression to be interpreted as a list by wrapping it
-    # in an extra set of list brackets. That form was supported for compatibility in
-    # v0.11, but is no longer supported in Terraform v0.12.
-    #
-    # If the expression in the following list itself returns a list, remove the
-    # brackets to avoid interpretation as a list of lists. If the expression
-    # returns a single list item then leave it as-is and remove this TODO comment.
     resources = [
       module.template_instance_role.role_arn,
     ]
@@ -336,15 +328,7 @@ data "aws_iam_policy_document" "codebuild_packer" {
     actions = [
       "iam:GetInstanceProfile",
     ]
-
-    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-    # force an interpolation expression to be interpreted as a list by wrapping it
-    # in an extra set of list brackets. That form was supported for compatibility in
-    # v0.11, but is no longer supported in Terraform v0.12.
-    #
-    # If the expression in the following list itself returns a list, remove the
-    # brackets to avoid interpretation as a list of lists. If the expression
-    # returns a single list item then leave it as-is and remove this TODO comment.
+    
     resources = [
       module.template_instance_role.instance_profile_arn,
     ]
